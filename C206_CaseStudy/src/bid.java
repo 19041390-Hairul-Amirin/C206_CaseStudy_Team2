9ce4a83c7b45314f0cdf10c81f45fe99ecaa996e
leehonwee <68002657+leehonwee@users.noreply.github.com>
2020-08-19 17:18:28 +0800

    Add files via upload

---
+++ C206_CaseStudy/src/bid.java
@@ -0,0 +1,48 @@
+
+
+
+public class bid{
+	private int bidId;
+	private String itemName;
+	private String sellerEmail;
+	private String buyerEmail;
+	private double bidPrice;
+	/**
+	 * @param bidId
+	 * @param itemName
+	 * @param sellerEmail
+	 * @param buyerEmail
+	 * @param bidPrice
+	 */
+	public bid(int bidId, String itemName, String sellerEmail, String buyerEmail, double bidPrice) {
+		super();
+		this.bidId = bidId;
+		this.itemName = itemName;
+		this.sellerEmail = sellerEmail;
+		this.buyerEmail = buyerEmail;
+		this.bidPrice = bidPrice;
+	}
+	public int getBidId() {
+		return bidId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public String getSellerEmail() {
+		return sellerEmail;
+	}
+	public String getBuyerEmail() {
+		return buyerEmail;
+	}
+	public double getBidPrice() {
+		return bidPrice;
+	}
+	public void setBidPrice(double x) {
+		this.bidPrice = x;
+	}
+	
+	
+	
+	
+	
+}
