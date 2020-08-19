4e802c7e1c1971c1883a0f8e42f7b3e39a97da47
Hairul <Hairul@DESKTOP-E7VUVOM>
2020-08-19 10:44:07 +0800

    Edited item class

--- C206_CaseStudy/src/AuctionShop.java
+++ C206_CaseStudy/src/AuctionShop.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 
 public class AuctionShop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Item> itemList = new ArrayList<Item>();
 	}
 
 }
--- C206_CaseStudy/src/Item.java
+++ C206_CaseStudy/src/Item.java
@@ -1,4 +1,71 @@
+import java.time.LocalDate;
 
 public class Item {
+	private int ID;
+	private String Description;
+	private String Price;
+	private String minIncrement;
+	private Boolean IsAvailable;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	
+	
 
+
+	public Item(int ID, String description, String price, String minIncrement, Boolean isAvailable, LocalDate startDate,
+			LocalDate endDate) {
+		super();
+		this.ID = ID;
+		this.Description = description;
+		Price = price;
+		this.minIncrement = minIncrement;
+		IsAvailable = isAvailable;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+
+	public int getID() {
+		return ID;
+	}
+
+
+	public String getDescription() {
+		return Description;
+	}
+
+
+
+	public String getPrice() {
+		return Price;
+	}
+
+
+	public String getMinIncrement() {
+		return minIncrement;
+	}
+
+
+
+	public Boolean getIsAvailable() {
+		return IsAvailable;
+	}
+
+
+	public void setIsAvailable(Boolean isAvailable) {
+		IsAvailable = isAvailable;
+	}
+	
+	
+	
 }
