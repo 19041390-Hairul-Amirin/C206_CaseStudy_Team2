27c16f6a34a9f1957c4ff84ac9f5156e20391bf5
19026820 <19026820@myrp.edu.sg>
2020-08-25 16:29:44 +0800

    bid med done

--- C206_CaseStudy/src/bidMethod.java
+++ C206_CaseStudy/src/bidMethod.java
@@ -65,7 +65,7 @@ public class bidMethod {
 		System.out.println("1. Display Bid");
 		System.out.println("2. Add Bid");
 		System.out.println("3. Delete Bid");
-		System.out.println("5. Search Bid");
+		System.out.println("4. Search Bid");
 		System.out.println("5. Update Bid");
 		Helper.line(80, "-");
 	}
@@ -85,7 +85,7 @@ public class bidMethod {
 		AuctionShop.setHeader("View Bid");
 		
 		for(int i =0 ; i<list1.size() ; i++){
-			String output = String.format(" %-10s %-10s %-10s %-10s %-10s\n", "BID ID", "ITEM NAME", "SELLER EMAIL",
+			String output = String.format("%-10s %-10s %-10s %-10s %-10s\n", "BID ID", "ITEM NAME", "SELLER EMAIL",
 					"BUYER EMAIL", "BID PRICE");
 			output += retrieveAll(list1);
 			System.out.println(output);
@@ -102,6 +102,8 @@ public class bidMethod {
 		
 		list1.add(new bid(id , name , semail , bemail , price));
 		
+		System.out.println("Bid Added!");
+		
 	}
 	
 	public static void deleteBid(ArrayList<bid> list1) {
@@ -109,31 +111,52 @@ public class bidMethod {
 		for(int i = 0 ; i<list1.size();i++) {
 			if(del == list1.get(i).getBidId()) {
 				list1.remove(i);
+				
+				System.out.println("Bid Deleted!");
 			}
 		}
 	}
 	public static void searchKeyword(ArrayList<bid> list1) {
 		
-		String info = Helper.readString("Enter Keyword: > " );
-		int info1 = Helper.readInt("Enter Bid id: > ");
-		double info2 = Helper.readDouble("Enter price: > ");
+		int option = Helper.readInt("Enter option: > ");
 		
-		for(int i=0 ; i<list1.size();i++) {
-			if(info == list1.get(i).getItemName()) {
-			System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s", list1.get(i).getBidId(), list1.get(i).getItemName(), list1.get(i).getSellerEmail(), list1.get(i).getBuyerEmail() ,list1.get(i).getBidPrice()));			
+		if(option == 1) {
+			String info = Helper.readString("Enter Keyword: > " );
+			for(int i=0 ; i<list1.size();i++) {
+				if(info.equalsIgnoreCase(list1.get(i).getItemName())){
+					System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s\n", "BID ID", "ITEM NAME", "SELLER EMAIL",
+							"BUYER EMAIL", "BID PRICE"));
+					System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s", list1.get(i).getBidId(), list1.get(i).getItemName(), list1.get(i).getSellerEmail(), list1.get(i).getBuyerEmail() ,list1.get(i).getBidPrice()));			
+				}
+				else if(info.equalsIgnoreCase(list1.get(i).getSellerEmail())) {
+					System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s\n", "BID ID", "ITEM NAME", "SELLER EMAIL",
+							"BUYER EMAIL", "BID PRICE"));
+					System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s", list1.get(i).getBidId(), list1.get(i).getItemName(), list1.get(i).getSellerEmail(), list1.get(i).getBuyerEmail() ,list1.get(i).getBidPrice()));					
+				}
 			}
-			else if(info == list1.get(i).getSellerEmail()) {
-				System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s", list1.get(i).getBidId(), list1.get(i).getItemName(), list1.get(i).getSellerEmail(), list1.get(i).getBuyerEmail() ,list1.get(i).getBidPrice()));					
+		}
+		else if(option == 2) {
+			int info1 = Helper.readInt("Enter Bid id: > ");
+			for(int i=0 ; i<list1.size();i++) {
+				if(info1 == list1.get(i).getBidId()) {
+					System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s\n", "BID ID", "ITEM NAME", "SELLER EMAIL",
+							"BUYER EMAIL", "BID PRICE"));
+					System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s", list1.get(i).getBidId(), list1.get(i).getItemName(), list1.get(i).getSellerEmail(), list1.get(i).getBuyerEmail() ,list1.get(i).getBidPrice()));			
 			}
-			else if(info1 == list1.get(i).getBidId()) {
-				System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s", list1.get(i).getBidId(), list1.get(i).getItemName(), list1.get(i).getSellerEmail(), list1.get(i).getBuyerEmail() ,list1.get(i).getBidPrice()));			
+		}	
+		}
+		else if(option == 3) {
+			double info2 = Helper.readDouble("Enter price: > ");
+			for(int i=0 ; i<list1.size();i++) {
+				if(info2 == list1.get(i).getBidPrice()) {
+					System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s\n", "BID ID", "ITEM NAME", "SELLER EMAIL",
+							"BUYER EMAIL", "BID PRICE"));
+					System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s", list1.get(i).getBidId(), list1.get(i).getItemName(), list1.get(i).getSellerEmail(), list1.get(i).getBuyerEmail() ,list1.get(i).getBidPrice()));			
+				}
 			}
-			else if(info1 == list1.get(i).getBidPrice()) {
-				System.out.println(String.format("%-10s %-10s %-10s %-10s %-10s", list1.get(i).getBidId(), list1.get(i).getItemName(), list1.get(i).getSellerEmail(), list1.get(i).getBuyerEmail() ,list1.get(i).getBidPrice()));			
-			}
-			else {
+		}
+		else {
 				System.out.println("Error!");
-			}
 		}
 		
 	}
@@ -146,6 +169,8 @@ public class bidMethod {
 		for(int i = 0; i < list1.size() ; i++) {
 			if(id == list1.get(i).getBidId()) {
 				list1.get(i).setBidPrice(upPrice);
+				
+				System.out.println("Price Updated!");
 			}
 			
 		}
